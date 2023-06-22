1acb3a97b2885ef5f174f8804a728841d65b02a2
shravan-lrm <shravan.vishwanathan@leftrightmind.com>
2023-06-22 12:28:32 +0530

    some changes

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# sbagpt
+# Sinequa AI Assistant
+
+Sinequa AI Assistant is a chatbot application capable of answering your questions about Sinequa SBA. It uses the latest language models from OpenAI and presents a user-friendly interface thanks to Streamlit.
+
+![Sinequa SBA AI Assistant](screenshot.png)
+
+## Getting Started
+
+Follow these steps to run Sinequa SBA AI Assistant on your local machine.
+
+### Prerequisites
+
+- Python 3.7+
+- [Streamlit](https://streamlit.io/)
+- [OpenAI](https://www.openai.com/)
+- Other Python dependencies from `requirements.txt`
+
+### Installation
+
+1. Clone the repository:
+-git clone https://github.com/your-username/sinequa-sba-ai-assistant.git
+-cd sinequa-sba-ai-assistant
+ 
+2. Install Python dependencies:
+-pip install -r requirements.txt
+
+
+3. Get your OpenAI API key from the [OpenAI website](https://www.openai.com/). You'll need to create an account and follow their instructions to generate an API key.
+
+4. Run the application:
+-streamlit run app.py
+
+
+5. The Streamlit app should now be running on your local machine. You'll see an URL in your terminal (usually `http://localhost:8501`), open that URL in your browser to use the application.
+
+6. Enter your OpenAI API key in the sidebar of the application and start chatting with the assistant!
+
+## Usage
+
+Enter your query into the text box and press enter. The AI Assistant will then use OpenAI's language model to answer your question based on the loaded PKL files. If you want to change the PKL files, simply replace the ones in the `data` folder with your own. These are generated using vector embedding models from OpenAI.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from dotenv import load_dotenv
 import pickle
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -9,39 +8,44 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
+import glob
 
 # Sidebar contents
 with st.sidebar:
     st.title('Chat with Sinequa SBA')
     st.markdown('''
     ## About
-    This app is an AI-driven assistant, capable of answering your questions about Sinequa SBA.
+    This app is an AI-driven assistant, capable of answering your questions about Sinequa SBA and sinequa platform.
     It utilizes the latest language models from OpenAI and presents a user-friendly interface thanks to Streamlit.
     Feel free to start a conversation with it!
     ''')
     st.write('[Sinequa SBA Documentation](https://sinequa.github.io/sba-angular/)')
 
 def main():
-    load_dotenv()
-    st.header("Sinequa SBA AI Assistant")
+    st.header("Sinequa AI Assistant")
 
     # Input field for OpenAI API key
     openai_api_key = st.sidebar.text_input("Enter your OpenAI API key:")
 
     # Check if the API key was entered
     if openai_api_key:
-        VectorStore = None
-        try:
-            with open("./sinequa_sba_documentation.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
-        except Exception as e:
-            st.error(f"An error occurred while initializing the assistant. Please make sure the documentation embeddings are available.")
-            return
+        # Get list of pkl files
+        pkl_files = glob.glob("./data/*.pkl")
+        
+        VectorStores = []
+        for pkl_file in pkl_files:
+            try:
+                with open(pkl_file, "rb") as f:
+                    VectorStores.append(pickle.load(f))
+            except Exception as e:
+                st.error(f"An error occurred while initializing the assistant from {pkl_file}. Please make sure the documentation embeddings are available.")
+                return
 
-        if VectorStore is not None:
-            # Accept user questions/query
-            query = st.text_input("How can I assist you today?")
-            if query:
+        # Accept user questions/query
+        query = st.text_input("How can I assist you today?")
+        if query:
+            best_response = None
+            for VectorStore in VectorStores:
                 try:
                     docs = VectorStore.similarity_search(query=query, k=3)
                     llm = OpenAI(temperature=0.7, max_tokens=300, verbose="true", openai_api_key=openai_api_key)
@@ -49,28 +53,35 @@ def main():
                     with get_openai_callback() as cb:
                         response = chain.run(input_documents=docs, question=query)
 
-                    if response is not None:
-                        if "`typescript" in response or "`cs" in response or "```html" in response:
-                            # Add syntax highlighting to the code.
-                            formatted_response = f"`typescript\n{response}\n`"
-                            st.markdown(formatted_resposne, unsafe_allow_html=True)
-                        else:
-                            # Generate a light grey box using HTML and CSS.
-                            box_style = """
-                            <style>
-                            .box {
-                                border: solid 1px #ddd;
-                                background-color: #f0f0f0;
-                                padding: 20px;
-                                margin: 20px 0;
-                                border-radius: 5px;
-                            }
-                            </style>
-                            """
-                            st.markdown(f"{box_style}<div class='box'>{response}</div>", unsafe_allow_html=True)
+                    # Choose the best response
+                    if response and (not best_response or len(response) > len(best_response)):
+                        best_response = response
                 except Exception as e:
-                    st.error(f"An error occurred while processing your question. Please try again. {e}")
+                    st.error(f"An error occurred while processing your question from one of the pkls. Please try again. {e}")
                     return
+
+            if best_response:
+                display_response(best_response)
+
+def display_response(response):
+    if "`typescript" in response or "`cs" in response or "```html" in response:
+        # Add syntax highlighting to the code.
+        formatted_response = f"`typescript\n{response}\n`"
+        st.markdown(formatted_response, unsafe_allow_html=True)
+    else:
+        # Generate a light grey box using HTML and CSS.
+        box_style = """
+        <style>
+        .box {
+            border: solid 1px #ddd;
+            background-color: #f0f0f0;
+            padding: 20px;
+            margin: 20px 0;
+            border-radius: 5px;
+        }
+        </style>
+        """
+        st.markdown(f"{box_style}<div class='box'>{response}</div>", unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
